5a8e6f9572ce1e5b3d194ec78d237e459f6953e2
hvalmer <hvalmer@gmail.com>
2020-11-10 16:34:50 -0300

    Finish catalog loaders

--- front-web/src/pages/Catalog/components/ProductDetails/index.tsx
+++ front-web/src/pages/Catalog/components/ProductDetails/index.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import { ReactComponent as ArrowIcon } from '../../../../core/assets/images/arrow.svg';
-import ProductPrice from '../../../../core/components/ProductPrice';
-import { Product } from '../../../../core/types/Product';
-import { makeRequest } from '../../../../core/utils/request';
+import { ReactComponent as ArrowIcon } from 'core/assets/images/arrow.svg';
+import ProductPrice from 'core/components/ProductPrice';
+import { Product } from 'core/types/Product';
+import { makeRequest } from 'core/utils/request';
+import ProductInfoLoader from '../Loaders/ProductInfoLoader';
+import ProductDescriptionLoader from '../Loaders/ProductDescriptionLoader';
 import './styles.scss';
 
 type ParamsType = {
@@ -13,38 +15,51 @@ type ParamsType = {
 const ProductDetails = () => {
     const { productId } = useParams<ParamsType>();
     const [product, setProduct] = useState<Product>();
-      
+    const [isLoading, setIsLoading] = useState(false);
+
+    console.log(isLoading);
+
     useEffect(() => {
-       makeRequest({url: `/products/${productId}`})
-        .then(response => setProduct(response.data));
+        setIsLoading(true);
+        makeRequest({ url: `/products/${productId}` })
+            .then(response => setProduct(response.data))
+            .finally(() => setIsLoading(false));
     }, [productId]);
 
-    return(
+    return (
         <div className="products-details-container">
             <div className="card-base border-radius-20 products-details">
-               <Link to="/products" className="products-details-goback">
-                    <ArrowIcon className="icon-goback"/>
+                <Link to="/products" className="products-details-goback">
+                    <ArrowIcon className="icon-goback" />
                     <h1 className="text-goback">voltar</h1>
-               </Link>
-               <div className="row">
+                </Link>
+                <div className="row">
                     <div className="col-6 pr-5">
-                        <div className="products-details-card text-center">
-                            <img src={product?.imgUrl} alt={product?.name} className="product-details-image"/>
-                        </div>
-                        <h1 className="product-details-name">
-                           {product?.name}
-                        </h1>
-                            {product?.price && <ProductPrice price={product?.price}/>}
+                        {isLoading ? <ProductInfoLoader/> : (
+                            <>
+                                <div className="products-details-card text-center">
+                                    <img src={product?.imgUrl} alt={product?.name} className="product-details-image" />
+                                </div>
+                                <h1 className="product-details-name">
+                                    {product?.name}
+                                </h1>
+                                {product?.price && <ProductPrice price={product?.price} />}
+                            </>
+                        )}
                     </div>
                     <div className="col-6 products-details-card">
-                        <h1 className="product-description-title">
-                            Descrição do Produto
-                        </h1>
-                        <p className="product-description-text">
-                            {product?.description}
-                        </p>
+                        {isLoading ? <ProductDescriptionLoader/> : (
+                            <>
+                                <h1 className="product-description-title">
+                                    Descrição do Produto
+                                </h1>
+                                <p className="product-description-text">
+                                    {product?.description}
+                                </p>
+                            </>
+                        )}
                     </div>
-               </div>
+                </div>
             </div>
         </div>
     );
